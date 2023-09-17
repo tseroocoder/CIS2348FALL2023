9e7a709a335623c166953bbfc4304a0e31f1dceb
tseroocoder <143126538+tseroocoder@users.noreply.github.com>
2023-09-16 22:49:55 -0500

    Added ZYLAB 1.20.py

---
+++ Zylab 1.20.py
@@ -0,0 +1,19 @@
+#Michelle Oteri
+# 2252197
+user_num = int(input("Enter integer:\n"))
+print("You entered:", user_num)
+
+
+user_num_squared = user_num * user_num
+user_num_cubed = user_num * user_num * user_num
+print(user_num, "squared is", user_num_squared)
+print("And", user_num, "cubed is", user_num_cubed, "!!")
+
+
+user_num1 = int(input("Enter another integer:\n"))
+
+
+sum_result = user_num + user_num1
+product_result = user_num * user_num1
+print(user_num, "+", user_num1, "is", sum_result)
+print(user_num, "*", user_num1, "is", product_result)
