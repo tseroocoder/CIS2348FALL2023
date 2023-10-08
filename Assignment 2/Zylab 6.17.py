53de7b3966959c1d0c5345622d7ff23cb2cc7974
tseroocoder <143126538+tseroocoder@users.noreply.github.com>
2023-10-08 01:08:31 -0500

    Added Zylab 6.17

---
+++ Assignment 2/Zylab 6.17.py
@@ -0,0 +1,67 @@
+#Michelle Oteri
+#2252197
+
+def make_stronger_password(password):
+    """Makes a simple password stronger by replacing characters and appending "q*s" to the end.
+
+
+
+
+
+
+
+    Args:
+
+
+
+      password: The simple password to make stronger.
+
+
+
+
+
+
+
+    Returns:
+
+
+
+      The stronger password.
+
+
+
+    """
+
+
+    replacements = {
+
+        "i": "!",
+
+        "a": "@",
+
+        "m": "M",
+
+        "B": "8",
+
+        "o": "."
+
+    }
+
+
+
+    for old_char, new_char in replacements.items():
+        password = password.replace(old_char, new_char)
+
+
+
+    password += "q*s"
+
+    return password
+
+
+
+password = input()
+
+stronger_password = make_stronger_password(password)
+
+print( stronger_password)
